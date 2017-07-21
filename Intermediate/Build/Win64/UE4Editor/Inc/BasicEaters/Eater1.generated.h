77fb60b874aa7374635b18aa5e67617c4ef534cf
Yug Chauhan <yugchauhan2000@gmail.com>
2017-07-21 11:49:17 -0400

    Field Setup

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/BasicEaters/Eater1.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BASICEATERS_Eater1_generated_h
+#error "Eater1.generated.h already included, missing '#pragma once' in Eater1.h"
+#endif
+#define BASICEATERS_Eater1_generated_h
+
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_RPC_WRAPPERS
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEater1(); \
+	friend BASICEATERS_API class UClass* Z_Construct_UClass_AEater1(); \
+public: \
+	DECLARE_CLASS(AEater1, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/BasicEaters"), NO_API) \
+	DECLARE_SERIALIZER(AEater1) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAEater1(); \
+	friend BASICEATERS_API class UClass* Z_Construct_UClass_AEater1(); \
+public: \
+	DECLARE_CLASS(AEater1, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/BasicEaters"), NO_API) \
+	DECLARE_SERIALIZER(AEater1) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEater1(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEater1) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEater1); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEater1); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEater1(AEater1&&); \
+	NO_API AEater1(const AEater1&); \
+public:
+
+
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEater1(AEater1&&); \
+	NO_API AEater1(const AEater1&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEater1); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEater1); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEater1)
+
+
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_PRIVATE_PROPERTY_OFFSET
+#define BasicEaters_Source_BasicEaters_Eater1_h_9_PROLOG
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_PRIVATE_PROPERTY_OFFSET \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_RPC_WRAPPERS \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_INCLASS \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BasicEaters_Source_BasicEaters_Eater1_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_PRIVATE_PROPERTY_OFFSET \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_INCLASS_NO_PURE_DECLS \
+	BasicEaters_Source_BasicEaters_Eater1_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BasicEaters_Source_BasicEaters_Eater1_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/BasicEaters/BasicEatersGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BasicEatersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICEATERS_API ABasicEatersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
