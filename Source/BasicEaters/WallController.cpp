2dc2abf3ad3c996d1618d9fd0381893f83ea64b2
hreeves9 <harvey_reeves9@icloud.com>
2017-07-31 16:07:19 -0400

    Fixed map issue

--- Source/BasicEaters/WallController.cpp
+++ Source/BasicEaters/WallController.cpp
@@ -9,10 +9,6 @@ AWallController::AWallController()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
-
-
-
 }
 
 
